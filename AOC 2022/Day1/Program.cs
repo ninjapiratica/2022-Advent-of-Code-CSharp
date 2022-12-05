c336fa69a86af901d38fb644b2fde221f04ab571
Chase Hanson <chase@entacit.com>
2022-12-05 15:59:45 -0800

    Day 1

---
+++ AOC 2022/Day1/Program.cs
@@ -0,0 +1,20 @@
+﻿
+var maxCalories = 0;
+
+var lastElfCalories = (await File.ReadAllLinesAsync("Input.txt"))
+    .Aggregate(0, (acc, line) =>
+    {
+        if (!string.IsNullOrWhiteSpace(line))
+        {
+            return acc + int.Parse(line);
+        }
+        else
+        {
+            maxCalories = Math.Max(maxCalories, acc);
+            return 0;
+        }
+    });
+
+maxCalories = Math.Max(maxCalories, lastElfCalories);
+
+Console.WriteLine($"Max Calories: {maxCalories}");
