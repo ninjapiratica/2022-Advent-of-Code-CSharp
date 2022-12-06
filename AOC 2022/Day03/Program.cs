1f6b7af1e6b4e388ebac5bcfd30806b036e11256
Chase Hanson <chase@entacit.com>
2022-12-05 17:26:35 -0800

    Day 3 Part 2

--- AOC 2022/Day03/Program.cs
+++ AOC 2022/Day03/Program.cs
@@ -1,5 +1,4 @@
-﻿
-var total = (await File.ReadAllLinesAsync("Input.txt"))
+﻿var total = (await File.ReadAllLinesAsync("Input.txt"))
     .Aggregate(0, (acc, line) =>
     {
         var part1 = line[..(line.Length / 2)];
@@ -11,3 +10,37 @@ var total = (await File.ReadAllLinesAsync("Input.txt"))
     });
 
 Console.WriteLine($"Part 1 Total: {total}");
+
+
+
+var badgePriority = (await File.ReadAllLinesAsync("Input.txt"))
+    .Aggregate(new Accumulator(), (acc, line) =>
+    {
+        acc.CurrentGroup.Add(line);
+
+        if (acc.CurrentGroup.Count < 3)
+        {
+            return acc;
+        }
+
+        var duplicated = acc.CurrentGroup[0]
+            .Join(acc.CurrentGroup[1], x => x, y => y, (x, y) => x)
+            .Join(acc.CurrentGroup[2], x => x, y => y, (x, y) => x)
+            .First();
+
+        acc.Priority += duplicated > 96 ? (duplicated - 96) : (duplicated - 38);
+
+        acc.CurrentGroup.Clear();
+
+        return acc;
+    })
+    .Priority;
+
+Console.WriteLine($"Part 1 Total: {badgePriority}");
+
+class Accumulator
+{
+    public int Priority { get; set; }
+
+    public List<string> CurrentGroup { get; } = new List<string>();
+}
