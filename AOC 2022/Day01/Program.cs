f412f5ec6d3e575e8aaef08e39738774273e08a6
Chase Hanson <chase@entacit.com>
2022-12-05 16:08:39 -0800

    Rename Day 1 Part 2

---
+++ AOC 2022/Day01/Program.cs
@@ -0,0 +1,41 @@
+﻿
+var maxCalories = 0;
+
+var lastElfCalories = (await File.ReadAllLinesAsync("Input.txt"))
+    .Aggregate(0, (acc, line) =>
+    {
+        if (!string.IsNullOrWhiteSpace(line))
+        {
+            return acc + int.Parse(line);
+        }
+        else
+        {
+            maxCalories = Math.Max(maxCalories, acc);
+            return 0;
+        }
+    });
+
+maxCalories = Math.Max(maxCalories, lastElfCalories);
+
+Console.WriteLine($"Part 1 Max Calories: {maxCalories}");
+
+
+
+var totalCalories = (await File.ReadAllLinesAsync("Input.txt"))
+    .Aggregate(new int[1] { 0 }, (acc, line) =>
+    {
+        if (!string.IsNullOrWhiteSpace(line))
+        {
+            acc[acc.Length - 1] += int.Parse(line);
+            return acc;
+        }
+        else
+        {
+            return acc.Append(0).ToArray();
+        }
+    })
+    .OrderByDescending(x => x)
+    .Take(3)
+    .Sum();
+
+Console.WriteLine($"Part 2 Total Calories: {totalCalories}");
