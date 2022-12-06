83cb6717d60f03c5523c86788f9451ce0d9bae15
Chase Hanson <chase@entacit.com>
2022-12-05 17:13:12 -0800

    Day 3 Part 1

---
+++ AOC 2022/Day03/Program.cs
@@ -0,0 +1,13 @@
+﻿
+var total = (await File.ReadAllLinesAsync("Input.txt"))
+    .Aggregate(0, (acc, line) =>
+    {
+        var part1 = line[..(line.Length / 2)];
+        var part2 = line[(line.Length / 2)..];
+
+        var duplicated = part1.Distinct().Concat(part2.Distinct()).GroupBy(x => x).Where(g => g.Count() > 1).First().Key;
+
+        return acc + (duplicated > 96 ? (duplicated - 96) : (duplicated - 38));
+    });
+
+Console.WriteLine($"Part 1 Total: {total}");
