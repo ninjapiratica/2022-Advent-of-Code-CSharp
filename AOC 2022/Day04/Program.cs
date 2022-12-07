04f9aecec5c33438adce946e14678d910e1cc5f6
Chase Hanson <chase@entacit.com>
2022-12-07 10:49:08 -0800

    Day 4 Part 1

---
+++ AOC 2022/Day04/Program.cs
@@ -0,0 +1,19 @@
+﻿var total = (await File.ReadAllLinesAsync("Input.txt"))
+    .Aggregate(0, (acc, line) =>
+    {
+        var parts = line.Split('-', ',').Select(x => int.Parse(x)).ToList();
+
+        if (parts[0] <= parts[2] && parts[1] >= parts[3])
+        {
+            return acc + 1;
+        }
+        else if (parts[2] <= parts[0] && parts[3] >= parts[1])
+        {
+            return acc + 1;
+        }
+
+        return acc;
+
+    });
+
+Console.WriteLine($"Part 1 Total: {total}");
