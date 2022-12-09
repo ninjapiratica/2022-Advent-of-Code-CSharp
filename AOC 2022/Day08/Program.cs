d31e023f8ee2c7d45740c757cf1164d4b67286c5
Chase Hanson <chase@entacit.com>
2022-12-09 06:09:55 -0800

    Day 8 Part 2

--- AOC 2022/Day08/Program.cs
+++ AOC 2022/Day08/Program.cs
@@ -84,9 +84,107 @@ for (int i = 0; i < lines.Length; i++)
 Console.WriteLine($"Part 1 Total Visible: {totalVisible}");
 
 
+for (int i = 0; i < lines.Length; i++)
+{
+    for (int j = 0; j < lines[i].Length; j++)
+    {
+        graph[i, j].Score = LookUp(graph[i, j], i, j) *
+            LookRight(graph[i, j], i, j) *
+            LookDown(graph[i, j], i, j) *
+            LookLeft(graph[i, j], i, j);
+    }
+}
+
+long highestScore = 0;
+for (int i = 0; i < lines.Length; i++)
+{
+    for (int j = 0; j < lines[0].Length; j++)
+    {
+        highestScore = Math.Max(graph[i, j].Score, highestScore);
+        Console.Write(graph[i, j].Score + "\t");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine($"Part 2 Scenic Score: {highestScore}");
+
+long LookUp(Tree tree, int i, int j)
+{
+    var count = 0;
+    while (i > 0)
+    {
+        count++;
+        if (graph[i - 1, j].Height < tree.Height)
+        {
+            i--;
+        }
+        else
+        {
+            break;
+        }
+    }
+    return count;
+}
+
+long LookRight(Tree tree, int i, int j)
+{
+    var count = 0;
+    while (j < lines[0].Length - 1)
+    {
+        count++;
+        if (graph[i, j + 1].Height < tree.Height)
+        {
+            j++;
+        }
+        else
+        {
+            break;
+        }
+    }
+    return count;
+}
+
+long LookDown(Tree tree, int i, int j)
+{
+    var count = 0;
+    while (i < lines.Length - 1)
+    {
+        count++;
+        if (graph[i + 1, j].Height < tree.Height)
+        {
+            i++;
+        }
+        else
+        {
+            break;
+        }
+    }
+    return count;
+}
+
+long LookLeft(Tree tree, int i, int j)
+{
+    var count = 0;
+    while (j > 0)
+    {
+        count++;
+        if (graph[i, j - 1].Height < tree.Height)
+        {
+            j--;
+        }
+        else
+        {
+            break;
+        }
+    }
+    return count;
+}
+
 class Tree
 {
     public int Height { get; set; }
 
     public bool? IsVisible { get; set; }
+
+    public long Score { get; set; }
 }
