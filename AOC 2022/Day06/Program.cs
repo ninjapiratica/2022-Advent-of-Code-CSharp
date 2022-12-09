89eac8e20960f824ca5f6242315a83bb7deac22b
Chase Hanson <chase@entacit.com>
2022-12-08 18:27:45 -0800

    Day 6 Part 1

---
+++ AOC 2022/Day06/Program.cs
@@ -0,0 +1,25 @@
+﻿
+var queue = new Queue<char>();
+
+var startOfPacket = (await File.ReadAllTextAsync("Input.txt"))
+    .TakeWhile(value =>
+    {
+        queue.Enqueue(value);
+
+        if (queue.Count > 4)
+        {
+            queue.Dequeue();
+        }
+
+        if (queue.GroupBy(x => x).Count() == 4)
+        {
+            return false;
+        }
+
+        return true;
+    })
+    .Count() + 1;
+
+Console.WriteLine($"Part 1 Start of Packet: {startOfPacket}");
+
+
