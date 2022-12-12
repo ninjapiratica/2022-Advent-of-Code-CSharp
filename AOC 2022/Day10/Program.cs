3e2c394958ffffdc33a38c6f0bb26244ac0ca1a7
Chase Hanson <chase@entacit.com>
2022-12-11 21:21:23 -0800

    Day 10 Part 2

--- AOC 2022/Day10/Program.cs
+++ AOC 2022/Day10/Program.cs
@@ -12,7 +12,7 @@ var aggFinal = (await File.ReadAllLinesAsync("Input.txt"))
             change = int.Parse(line.Split(' ')[1]);
         }
 
-        Console.WriteLine($"{line}\t{agg.ImportantTotal}\t{agg.Total}\t{string.Join(", ", cycles)}");
+        Console.WriteLine($"{line}\t\t{agg.Total}\t{string.Join(", ", cycles)}");
 
         agg.ImportantTotal += importantCycles.Intersect(cycles).DefaultIfEmpty().Sum(c => c * agg.Total);
         agg.CycleNumber = cycles.Max() + 1;
@@ -22,6 +22,39 @@ var aggFinal = (await File.ReadAllLinesAsync("Input.txt"))
     });
 
 Console.WriteLine($"Part 1 Output: {aggFinal.ImportantTotal}");
+
+
+(await File.ReadAllLinesAsync("Input.txt"))
+    .Aggregate(new Agg { Total = 1, CycleNumber = 1 }, (agg, line) =>
+    {
+        var cycles = new List<int> { agg.CycleNumber };
+        var change = 0;
+        if (line != "noop")
+        {
+            cycles.Add(agg.CycleNumber + 1);
+            change = int.Parse(line.Split(' ')[1]);
+        }
+
+        //Console.WriteLine($"{line}\t{agg.ImportantTotal}\t{agg.Total}\t{string.Join(", ", cycles)}");
+        foreach (var cycle in cycles)
+        {
+            var position = (cycle % 40) - 1;
+
+            if (position == 0)
+            {
+                Console.WriteLine();
+            }
+
+            Console.Write(agg.Total - 1 <= position && position <= agg.Total + 1 ? '#' : '.');
+        }
+
+        agg.ImportantTotal += importantCycles.Intersect(cycles).DefaultIfEmpty().Sum(c => c * agg.Total);
+        agg.CycleNumber = cycles.Max() + 1;
+        agg.Total += change;
+
+        return agg;
+    });
+
 
 class Agg
 {
