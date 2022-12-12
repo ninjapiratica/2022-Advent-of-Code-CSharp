ac62a908edaf0d22590db0752a082110de86f461
Chase Hanson <chase@entacit.com>
2022-12-12 04:28:08 -0800

    Day 12 Part 2

--- AOC 2022/Day12/Program.cs
+++ AOC 2022/Day12/Program.cs
@@ -13,13 +13,11 @@ for (int i = 0; i < maxY; i++)
 {
     for (int j = 0; j < maxX; j++)
     {
-        int? shortestPath = null;
         var value = lines[i][j];
         if (value == 'S')
         {
             value = 'a';
             start = new Point(j, i);
-            shortestPath = 0;
         }
         else if (value == 'E')
         {
@@ -27,41 +25,82 @@ for (int i = 0; i < maxY; i++)
             end = new Point(j, i);
         }
 
-        grid[j, i] = new Coordinate { Height = value, ShortestPath = shortestPath };
+        grid[j, i] = new Coordinate { Height = value };
     }
 }
 
-var current = start;
+var sToE = RunSimulation(start);
 
-var steppers = new List<Stepper?>
-{
-    new Stepper(null, start)
-};
+Console.WriteLine(sToE);
 
-while (!steppers.Any(s => s.Current == end))
+var shortestPath = int.MaxValue;
+for (int i = 0; i < maxY; i++)
 {
-    foreach (var stepper in steppers.ToList())
+    for (int j = 0; j < maxX; j++)
     {
-        stepper?.Step(steppers, grid);
+        if (grid[j, i].Height == 'a')
+        {
+            Restart();
+            shortestPath = Math.Min(shortestPath, RunSimulation(new Point(j, i)));
+
+        }
     }
 }
 
-var xStepper = steppers.First(s => s.Current == end);
+Console.WriteLine(shortestPath);
 
-for (int i = 0; i < grid.GetLength(1); i++)
+
+
+int RunSimulation(Point startingPoint)
 {
-    for (int j = 0; j < grid.GetLength(0); j++)
+    var steppers = new List<Stepper?>
     {
-        Console.Write(xStepper.Path.Contains(new Point(j, i)) ? 'x' : '.');
+        new Stepper(null, startingPoint)
+    };
+
+    grid[startingPoint.X, startingPoint.Y].ShortestPath = 0;
+
+    while (steppers.Any() && !steppers.Any(s => s.Current == end))
+    {
+        foreach (var stepper in steppers.ToList())
+        {
+            stepper?.Step(steppers, grid);
+        }
     }
-    Console.WriteLine();
+
+    var xStepper = steppers.FirstOrDefault(s => s.Current == end);
+
+    var pathLength = xStepper != null ? xStepper.Path.Count() - 1 : int.MaxValue;
+
+    if (pathLength == 525)
+    {
+        for (int i = 0; i < grid.GetLength(1); i++)
+        {
+            for (int j = 0; j < grid.GetLength(0); j++)
+            {
+                Console.Write(xStepper.Path.Contains(new Point(j, i)) ? 'x' : '.');
+            }
+
+            Console.WriteLine();
+        }
+    }
+
+    // steps is total path length - starting location
+    return pathLength;
 }
 
-// steps is total path length - starting location
-Console.WriteLine($"Part 1 Result: {steppers.First(s => s.Current == end)?.Path.Count() - 1}");
-// 534
 
 
+void Restart()
+{
+    for (int i = 0; i < grid.GetLength(1); i++)
+    {
+        for (int j = 0; j < grid.GetLength(0); j++)
+        {
+            grid[j, i].ShortestPath = null;
+        }
+    }
+}
 
 
 
