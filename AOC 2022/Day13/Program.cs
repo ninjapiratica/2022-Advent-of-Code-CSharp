e3ff3b6f77671389e0bf00366a95a299dd87c511
Chase Hanson <chase@entacit.com>
2022-12-12 22:02:33 -0800

    Day 13 Part 2

--- AOC 2022/Day13/Program.cs
+++ AOC 2022/Day13/Program.cs
@@ -3,6 +3,11 @@ var lines = await File.ReadAllLinesAsync("Input.txt");
 
 var index = 0;
 var sum = 0;
+
+var dividerPacket1 = Parse("[[2]]");
+var dividerPacket2 = Parse("[[6]]");
+
+var allPackets = new List<Context> { dividerPacket1, dividerPacket2 };
 for (int i = 0; i < lines.Length; i += 3)
 {
     index++;
@@ -11,9 +16,25 @@ for (int i = 0; i < lines.Length; i += 3)
     var line1 = Parse(lines[i]);
     var line2 = Parse(lines[i + 1]);
     sum += Compare(line1, line2, index) ?? 0;
+
+    allPackets.Add(line1);
+    allPackets.Add(line2);
 }
 
 Console.WriteLine(sum);
+
+allPackets.Sort(new ContextComparer());
+
+foreach (var item in allPackets)
+{
+    Console.WriteLine(item);
+}
+
+var index1 = allPackets.IndexOf(dividerPacket1) + 1;
+var index2 = allPackets.IndexOf(dividerPacket2) + 1;
+
+Console.WriteLine(index1 * index2);
+
 
 int? Compare(Context ctx1, Context ctx2, int index)
 {
@@ -130,4 +151,89 @@ Context Parse(string v)
 class Context : List<object>
 {
     public Context? Parent { get; set; }
+
+    public override string ToString()
+    {
+        return $"[{string.Join(",", this)}]";
+    }
+}
+
+class ContextComparer : IComparer<Context>
+{
+    public int Compare(Context? ctx1, Context? ctx2)
+    {
+        for (int j = 0; j < ctx1.Count; j++)
+        {
+            if (ctx2.Count == j)
+            {
+                return 1;
+            }
+
+            if (ctx1[j] is int && ctx2[j] is int)
+            {
+                if ((int)ctx1[j] > (int)ctx2[j])
+                {
+                    return 1;
+                }
+                else if ((int)ctx1[j] < (int)ctx2[j])
+                {
+                    return -1;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+            if (ctx1[j] is Context && ctx2[j] is Context)
+            {
+                var result = Compare((Context)ctx1[j], (Context)ctx2[j]);
+
+                if (result != 0)
+                {
+                    return result;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+            if (ctx1[j] is Context)
+            {
+                var ctx2Context = new Context { ctx2[j] };
+
+                var result = Compare((Context)ctx1[j], ctx2Context);
+                if (result != 0)
+                {
+                    return result;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            else
+            {
+                var ctx1Context = new Context { ctx1[j] };
+
+                var result = Compare(ctx1Context, (Context)ctx2[j]);
+                if (result != 0)
+                {
+                    return result;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
+
+        if (ctx2.Count > ctx1.Count)
+        {
+            return -1;
+        }
+
+        return 0;
+    }
 }
